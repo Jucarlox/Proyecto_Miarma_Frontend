e3086f700ca74001453565ff9d5e4428c23e5194
ardana.mujua21 <cardamu02@gmail.com>
2022-02-16 12:34:35 +0100

    Progresos

---
+++ flutter_miarmapp/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_miarmapp/screens/home_screen.dart';
+import 'package:flutter_miarmapp/screens/login_screen.dart';
+import 'package:flutter_miarmapp/screens/menu_screen.dart';
+import 'package:flutter_miarmapp/screens/register_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MiarmApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/home': (context) => const MenuScreen(),
+      },
+    );
+  }
+}
